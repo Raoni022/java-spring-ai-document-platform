53f07ebc18272a9b22b7296e3110d1af9b923a59
Raoni Medeiros <62862446+Raoni022@users.noreply.github.com>
2026-04-18 15:26:09 -0300

    Add Spring Boot application entry point

---
+++ src/main/java/com/raoni/legalrag/LegalRagApplication.java
@@ -0,0 +1,11 @@
+package com.raoni.legalrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalRagApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LegalRagApplication.class, args);
+    }
+}
